80411b091ce53cb196c9368b3b0cda2a313be7af
davronovuz <davronovtatu@gmail.com>
2025-04-09 12:29:16 +0500

    ozgarishlasr

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Django migrations’larni bajarish
-python manage.py makemigrations
-python manage.py migrate
-
-# Gunicorn serverini ishga tushirish
-exec gunicorn --bind 0.0.0.0:8005 config.wsgi:application
