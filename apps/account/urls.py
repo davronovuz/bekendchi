d7bb7154aa049fc660a83b863c518ae34eb3bd8e
kali <kali@kali.kali>
2025-04-13 18:16:20 +0500

    loyiha ishga  tushdeeesssddsei

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -4,7 +4,7 @@ from . import views
 handler404 = views.custom_404  # Maxsus 404 handler
 
 urlpatterns = [
-    path('signup/', views.SignupView.as_view(), name='signup'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.SignupView.as_view(), name='sn'),
+    path('login/', views.LoginView.as_view(), name='ln'),
+    path('logout/', views.LogoutView.as_view(), name='lt'),
 ]
