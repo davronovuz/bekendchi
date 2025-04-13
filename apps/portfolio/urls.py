14df608fa371668ed124a97c85a6f1b0f9b43a41
kali <kali@kali.kali>
2025-04-13 20:28:11 +0500

    loyiha ishga  tushdeeesssddsssssssssei

--- apps/portfolio/urls.py
+++ apps/portfolio/urls.py
@@ -1,5 +1,7 @@
 
 from .views import portfolio_view  # `your_app` ni loyihangizdagi app nomi bilan almashtiring
+from django.urls import path
+
 
 urlpatterns = [
     path('haqida/', portfolio_view, name='haqimda'),
