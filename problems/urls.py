745aa4f0300a81e760385f75122aa3cc58592054
davronovuz <davronovtatu@gmail.com>
2025-04-14 13:37:28 +0500

    masalalar bo'limi qo'shildii

--- problems/urls.py
+++ problems/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import problem_list, problem_detail, submit_code
 
 urlpatterns = [
     path('', problem_list, name='problem_list'),
-    path('<int:pk>/', problem_detail, name='problem_detail'),
-    path('<int:pk>/submit/', submit_code, name='submit_code'),
+    # UUID uchun moslashtirilgan URL pattern
+    re_path(r'^(?P<pk>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$', problem_detail, name='problem_detail'),
+    re_path(r'^(?P<pk>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/submit/$', submit_code, name='submit_code'),
 ]
